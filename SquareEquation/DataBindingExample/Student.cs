dc7021b19e2e67c63b68a65db63a439f658aab6e
Zorin Ivan <zorin.ivan@outlook.com>
2015-10-09 23:59:57 +0300

    Пример биндинга доделал

---
+++ SquareEquation/DataBindingExample/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingExample
+{
+    public class Exam
+    {
+        public String Name { get; set; }
+        public Byte Mark { get; set; }
+        public DateTime? PassedDate { get; set; }
+
+        public Exam(String name, Byte mark, DateTime? passed) 
+        {
+            Name = name;
+            Mark = mark;
+            PassedDate = passed;
+        }
+    }
+
+    public class Student
+    {
+        public String Name { get; set; }
+        public String MarkbookNumber { get; set; }
+
+        public List<Exam> Exams { get; set; }
+
+        public Student(String name, String markbook)
+        {
+            Exams = new List<Exam>();
+            Exams.Add(new Exam("Дискретная математика", 4, DateTime.Parse("12.01.2015")));
+            Exams.Add(new Exam("Программирование", 0, null));
+            Exams.Add(new Exam("Математический анализ", 5, DateTime.Parse("15.01.2015")));
+            Name = name;
+            MarkbookNumber = markbook;
+        }
+    }
+}
---
+++ SquareEquation/DataBindingExample/ViewModels.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace DataBindingExample
+{
+    public class ExamViewModel : INotifyPropertyChanged
+    {
+        public String Name { get; set; }
+        public DateTime Passed { get; set; }
+        protected Byte _mark;
+        public Byte Mark
+        {
+            get { return _mark; }
+            set
+            {
+                _mark = value;
+                Color = (_mark > 2) ? "Green" : "Red";
+            }
+        }
+
+        protected String _color;
+        public String Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("Color");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(String property)
+        {
+            var ev = PropertyChanged;
+            if (ev != null)
+            {
+                ev(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public ExamViewModel(Exam model)
+        {
+            this.Name = model.Name;
+            this.Passed = (model.PassedDate != null) ? model.PassedDate.Value : DateTime.Now;
+            this.Mark = model.Mark;
+        }
+    }
+
+    public class StudentViewModel
+    {
+        public String Name { get; set; }
+        public String Markbook { get; set; }
+
+        public ObservableCollection<ExamViewModel> Exams { get; set; }
+
+        public StudentViewModel(Student student)
+        {
+            Name = student.Name;
+            Markbook = student.MarkbookNumber;
+
+            Exams = new ObservableCollection<ExamViewModel>();
+            foreach (var e in student.Exams)
+            {
+                Exams.Add(new ExamViewModel(e));
+            }
+        }
+    }
+}
